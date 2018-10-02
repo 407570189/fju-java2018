d91ebba57ab2833bcb84d7c8861f7df2d892db5c
user <user@es302n03>
2018-10-02 11:08:14 +0800

    chang

--- myproject/src/com/tom/exam.java
+++ myproject/src/com/tom/exam.java
@@ -3,7 +3,21 @@ package com.tom;
 public class exam {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Hello introduce myself
+				int age= 19;
+		        String firstname ="Hung";
+		        String lastname = "Shang";
+		        String message = "hello";
+		        String aaa = "everybody";
+		        
+		        float weight =(float)58.5;
+		        float height = 176.5f;
+		        System.out.println(message);
+				System.out.println("my name is");
+		        System.out.println(firstname+lastname);
+		        System.out.println(age+"years old");
+				System.out.println("tall"+height);
+				System.out.println("weight"+weight);
 
 	}
 

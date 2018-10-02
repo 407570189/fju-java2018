905e07c8e56277985839c6b5cf8d437bc1410c42
user <user@es302n03>
2018-10-02 11:12:21 +0800

    chang3

---
+++ exam/src/com/fju/Hello.java
@@ -0,0 +1,23 @@
+package com.fju;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		//Hello introduce myself
+		int age= 19;
+        String firstname ="Hung";
+        String lastname = "Shang";
+        String message = "hello";
+        String aaa = "everybody";
+        
+        float weight =(float)58.5;
+        float height = 176.5f;
+        System.out.println(message+aaa);
+		System.out.println("my name is");
+        System.out.println(firstname+lastname);
+        System.out.println(age+"years old");
+		System.out.println("tall"+height);
+		System.out.println("weight"+weight);
+	}
+
+}

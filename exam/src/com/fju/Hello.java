0b7ab82474821403b61ed2a150d45ff7174af0f9
user <user@es302n03>
2018-10-02 11:32:02 +0800

    vairable

--- exam/src/com/fju/Hello.java
+++ exam/src/com/fju/Hello.java
@@ -4,20 +4,24 @@ public class Hello {
 
 	public static void main(String[] args) {
 		//Hello introduce myself
-		int age= 19;
+		int age= 18;
         String firstname ="Hung";
         String lastname = "Shang";
         String message = "hello";
         String aaa = "everybody";
-        
         float weight =(float)58.5;
-        float height = 176.5f;
+        float height = 1.765f;
+        float bmi = weight / (height*height);
         System.out.println(message+aaa);
 		System.out.println("my name is");
         System.out.println(firstname+lastname);
         System.out.println(age+"years old");
 		System.out.println("tall"+height);
 		System.out.println("weight"+weight);
+		System.out.println(bmi);
+		age = age+1;
+		System.out.println(age);
+
 	}
 
 }
